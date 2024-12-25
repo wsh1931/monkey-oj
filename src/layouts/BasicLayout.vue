7c33ff050c2aee3903e65490f2020c64465a5db3
wsh <1931443283@qq.com>
2024-12-25 14:59:05 +0800

    完成vue3初始化, 路由导航功能,引入ArcoDesign

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/wsh1931">花果山OJ</a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basicLayout {
+}
+
+.header {
+  background-color: lightcyan;
+  margin-bottom: 16px;
+}
+
+.content {
+  background-color: lightblue;
+  margin-bottom: 16px;
+}
+
+.footer {
+  background-color: lightcoral;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import router from "./router";
+import store from "./store";
+import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
+import App from "./App.vue";
+import "@arco-design/web-vue/dist/arco.css";
+
+createApp(App).use(ArcoVue).use(store).use(router).mount("#app");
